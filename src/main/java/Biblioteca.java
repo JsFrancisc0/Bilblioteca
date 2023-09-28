50aad4404f4a0c44a9f570730bf1cb6adb1d0d51
JsFrancisc0 <j.saldias03@ufromail.cl>
2023-09-28 15:01:37 -0300

    avance en la clase Biblioteca

--- src/main/java/Biblioteca.java
+++ src/main/java/Biblioteca.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class Biblioteca {
+
+
+    ArrayList listaLibros;
+    String nombreBiblioteca, direccion;
+
+    Biblioteca(ArrayList listaLibros, String nombreBiblioteca, String direccion){
+
+        this.listaLibros = listaLibros;
+        this.nombreBiblioteca = nombreBiblioteca;
+        this.direccion = direccion;
+
+    }
+
+
 }
